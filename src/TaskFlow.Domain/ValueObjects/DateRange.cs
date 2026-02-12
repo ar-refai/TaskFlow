37e3307f3792bb3e23abd05f02308db1cdb02ddd
AbdelRahman Elrefai <team.man00@gmail.com>
2026-02-12 13:05:26 +0200

    D: Add DateRange value object with validation

---
+++ src/TaskFlow.Domain/ValueObjects/DateRange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Domain.ValueObjects
+{
+    public readonly record struct DateRange
+    {
+        public DateTime StartDate { get; init; }
+        public DateTime DueDate { get; init; }
+
+        public DateRange(DateTime startDate, DateTime dueDate)
+        {
+            if (dueDate > startDate) throw new ArgumentException("Start date must be before due date.", nameof(startDate));
+            StartDate = startDate;
+            DueDate = dueDate;
+        }
+
+        public int DurationInDays => (DueDate - StartDate).Days;
+    }
+}
