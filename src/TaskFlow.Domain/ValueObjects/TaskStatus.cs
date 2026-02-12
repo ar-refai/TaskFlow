a9e7931ba5a0d31afcb49e25bfb0b305651676d9
AbdelRahman Elrefai <team.man00@gmail.com>
2026-02-12 12:41:14 +0200

    C: Add enum value objects (TaskStatus, Priority)

---
+++ src/TaskFlow.Domain/ValueObjects/TaskStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Domain.ValueObjects
+{
+    public enum TaskStatus
+    {
+        ToDo,
+        InProgress,
+        InReview,
+        Done,
+        Cancelled,
+    }
+}
