ea2ae3efc01919386de1e43280f9db73d30859b6
AbdelRahman Elrefai <team.man00@gmail.com>
2026-02-12 13:18:19 +0200

    E: Add Tag value Object with validation.

---
+++ src/TaskFlow.Domain/ValueObjects/Tag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Domain.ValueObjects
+{
+    public readonly record struct Tag
+    {
+        public string Value { get; init; }
+        public Tag(string value)
+        {
+            if (string.IsNullOrEmpty(Value)) throw new ArgumentException("Tag can't be empty or whitespace.", nameof(value));
+            if (value.Length > 30) throw new ArgumentException("Tag cannot excced 30 characters.", nameof(value));
+            Value = value.Trim();
+        }
+        public static implicit operator string(Tag tag) => tag.Value;
+    }
+}
