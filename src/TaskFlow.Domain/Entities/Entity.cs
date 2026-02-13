1f6a97e6fccacd04c035ee41fe973403b1e1c5c9
AbdelRahman Elrefai <team.man00@gmail.com>
2026-02-13 10:35:39 +0200

    H: Add Entity base class with domain event support and ID-based equality

---
+++ src/TaskFlow.Domain/Entities/Entity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskFlow.Domain.Events;
+
+namespace TaskFlow.Domain.Entities
+{
+    public abstract class Entity<TId> where TId : notnull
+    {
+        public abstract TId Id { get; protected set; }
+        private readonly List<IDomainEvent> _domainEvents = new();
+        public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly<IDomainEvent>(); 
+
+        protected void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not Entity<TId> other) return false; 
+            if (ReferenceEquals(this, other)) return true;
+            if (GetType() != other.GetType()) return false;
+            return Id.Equals(other.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public static bool operator ==(Entity<TId>? left, Entity<TId>? right)
+        {
+            return left is null ? right is null : left.Equals(right);
+        }
+
+        public static bool operator !=(Entity<TId>? left, Entity<TId>? right)
+        {
+            return !(left == right);
+        }
+
+
+    }
+}
